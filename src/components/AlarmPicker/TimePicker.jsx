a537e30730c8b5f005db456df7b03efe9cd22763
MeetYourCreator <rzshamir@icloud.com>
2024-07-16 18:20:57 -0400

    add logic to set alarm

--- src/components/AlarmPicker/TimePicker.jsx
+++ src/components/AlarmPicker/TimePicker.jsx
@@ -1,13 +1,29 @@
-import React, {useState} from 'react';
-import {TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {TouchableOpacity, Alert} from 'react-native';
 import DatePicker from 'react-native-date-picker';
 import {useStyles} from '../../hooks/useStyles';
 
 const TimePicker = () => {
   const [isOpen, setIsOpen] = useState(true);
-  const [time, setTime] = useState(new Date());
-
+  const [alarmTime, setAlarmTime] = useState(new Date());
   const styles = useStyles();
+
+  console.log('alarmTime: ', alarmTime);
+
+  //useEffect(() => {
+  //  const checkAlarm = setInterval(() => {
+  //    const currentTime = new Date();
+  //    if (
+  //      currentTime.getHours() === alarmTime.getHours() &&
+  //      currentTime.getMinutes() === alarmTime.getMinutes()
+  //    ) {
+  //      Alert.alert('Alarm');
+  //      clearInterval(checkAlarm);
+  //    }
+  //  }, 4000);
+  //  return () => clearInterval(checkAlarm);
+  //}, [alarmTime]);
+
   return (
     <>
       <TouchableOpacity
@@ -17,10 +33,10 @@ const TimePicker = () => {
       />
       <DatePicker
         open={isOpen}
-        date={time}
+        date={alarmTime}
         onConfirm={(value) => {
           setIsOpen(false);
-          setTime(value);
+          setAlarmTime(value);
         }}
         onCancel={() => setIsOpen(false)}
         title="Alarm"
