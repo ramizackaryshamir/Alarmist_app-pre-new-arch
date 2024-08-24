7ab3cda7b974a918e5045c289f319fbf7b1681f8
RamiZackaryShamir <rzshamir@icloud.com>
2024-08-24 10:26:10 -0400

    stack group screens

--- App.js
+++ App.js
@@ -8,75 +8,82 @@ import AlarmSettingsSoundOptionsScreen from './src/components/AlarmPicker/screen
 import DigitalClockScreen from './src/screens/DigitalClockScreen';
 import ScreenClockScreen from './src/screens/ScreenClockScreen';
 import Menu from './src/components/Menu';
-import {TouchableOpacity, Text, Button} from 'react-native';
+import {Button} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerStyle: {backgroundColor: '#00FFFF'},
-            headerTintColor: '#00FFFF',
-            headerShown: true,
-          }}
-        />
-        <Stack.Screen
-          name="Alarm Settings Screen"
-          component={AlarmSettingsScreen}
-          options={(navigation, route) => ({
-            headerStyle: {backgroundColor: 'red'},
-            //headerTintColor: '#F2D935',
-            headerBackTitle: 'Cancel',
-            headerShown: true,
-            headerRight: () => <Button title="Save" />,
-          })}
-        />
-        <Stack.Screen
-          name="Repeat"
-          component={AlarmSettingsRepeatOptionScreen}
-          options={(navigation, route) => ({
-            headerStyle: {backgroundColor: 'green'},
-            //headerTintColor: '#F2D935',
-            headerShown: true,
-            headerLeft: () => <Button title="Back" />,
-          })}
-        />
-        <Stack.Screen
-          name="Sound"
-          component={AlarmSettingsSoundOptionsScreen}
-          options={{
-            headerStyle: {backgroundColor: '#F2D935'},
-            //headerTintColor: '#F2D935',
-            headerShown: true,
-          }}
-        />
-        <Stack.Screen
-          name="Digital Clock"
-          component={DigitalClockScreen}
-          options={{
-            headerStyle: {
-              backgroundColor: '#F2D935',
-            },
-            headerTintColor: '#F2D935',
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Screen Clock"
-          component={ScreenClockScreen}
-          options={{
-            headerStyle: {
-              backgroundColor: '#F2D935',
-            },
-            headerTintColor: '#F2D935',
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="Menu" component={Menu} />
+      <Stack.Navigator>
+        <Stack.Group>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerStyle: {backgroundColor: '#00FFFF'},
+              headerTintColor: '#00FFFF',
+              headerShown: true,
+            }}
+          />
+          <Stack.Screen
+            name="Digital Clock"
+            component={DigitalClockScreen}
+            options={{
+              headerStyle: {
+                backgroundColor: '#F2D935',
+              },
+              headerTintColor: '#F2D935',
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Screen Clock"
+            component={ScreenClockScreen}
+            options={{
+              headerStyle: {
+                backgroundColor: '#F2D935',
+              },
+              headerTintColor: '#F2D935',
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="Menu" component={Menu} />
+        </Stack.Group>
+        <Stack.Group
+        //screenOptions={{presentation: 'modal'}}
+        >
+          <Stack.Screen
+            name="Alarm Settings Screen"
+            component={AlarmSettingsScreen}
+            options={(navigation, route) => ({
+              headerStyle: {backgroundColor: 'red'},
+              headerBackTitle: 'Cancel',
+              headerShown: true,
+              headerRight: () => <Button title="Save" />,
+              //headerTintColor: '#F2D935',
+              //presentation: 'modal',
+            })}
+          />
+          <Stack.Screen
+            name="Repeat"
+            component={AlarmSettingsRepeatOptionScreen}
+            options={(navigation, route) => ({
+              headerStyle: {backgroundColor: 'green'},
+              headerShown: true,
+              headerLeft: () => <Button title="Back" />,
+              //headerTintColor: '#F2D935',
+            })}
+          />
+          <Stack.Screen
+            name="Sound"
+            component={AlarmSettingsSoundOptionsScreen}
+            options={{
+              headerStyle: {backgroundColor: '#F2D935'},
+              headerShown: true,
+              //headerTintColor: '#F2D935',
+            }}
+          />
+        </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   );
