23f9613b1c481dd45b34459d2906f111cf452956
RamiZackaryShamir <rami.zackary.shamir@icloud.com>
2024-11-11 12:20:30 -0500

    optimize useStyles hooks with useMemo

--- src/components/AlarmPicker/components/useStyles.ts
+++ src/components/AlarmPicker/components/useStyles.ts
@@ -1,3 +1,4 @@
+import {useMemo} from 'react';
 import {StyleSheet} from 'react-native';
 import {Colors} from '../../../lib/Colors';
 import {useDarkMode} from '../../../hooks/useDarkMode';
@@ -5,31 +6,35 @@ import {useDarkMode} from '../../../hooks/useDarkMode';
 export const useStyles = () => {
   const {theme} = useDarkMode();
 
-  const styles = StyleSheet.create({
-    bottomSheetRowView: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      height: 50,
-      paddingLeft: 5,
-      paddingRight: 5,
-      backgroundColor:
-        theme === 'dark' ? Colors.blackPurple3 : Colors.vibrantWhite3,
-    },
-    bottomSheetText: {
-      color: theme === 'dark' ? Colors.white : Colors.blackPurple1,
-    },
-    alarmSettingsRepeatOption: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-      height: 40,
-      paddingRight: 10,
-      paddingLeft: 10,
-      marginBottom: 5,
-      backgroundColor: 'green',
-    },
-  });
+  const styles = useMemo(
+    () =>
+      StyleSheet.create({
+        bottomSheetRowView: {
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: 50,
+          paddingLeft: 5,
+          paddingRight: 5,
+          backgroundColor:
+            theme === 'dark' ? Colors.blackPurple3 : Colors.vibrantWhite3,
+        },
+        bottomSheetText: {
+          color: theme === 'dark' ? Colors.white : Colors.blackPurple1,
+        },
+        alarmSettingsRepeatOption: {
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+          height: 40,
+          paddingRight: 10,
+          paddingLeft: 10,
+          marginBottom: 5,
+          backgroundColor: 'green',
+        },
+      }),
+    [theme],
+  );
   return styles;
 };
